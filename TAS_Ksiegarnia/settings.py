90c77faf15a7b2962303c80e6e823139e309b003
endablju <simaosrz@gmail.com>
2014-11-15 14:25:45 +0100

    mysql
    baza danych w mysql na serwerze db4freenet

--- TAS_Ksiegarnia/settings.py
+++ TAS_Ksiegarnia/settings.py
@@ -67,8 +67,13 @@ WSGI_APPLICATION = 'TAS_Ksiegarnia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tasksiegarnia',
+	    'USER': 'ksiegarnia',
+        'PASSWORD': 'tas_projekt',
+		'HOST': 'db4free.net',                      
+
+		
     }
 }
 
